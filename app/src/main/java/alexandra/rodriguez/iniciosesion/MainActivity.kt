c15ca4b05e7dcb85b304bd5f5f2aa8804dd59da3
Alexandra <102197498+AlexandraRodCas@users.noreply.github.com>
2023-03-11 22:55:35 -0700

    Initial commit

---
+++ app/src/main/java/alexandra/rodriguez/iniciosesion/MainActivity.kt
@@ -0,0 +1,93 @@
+package alexandra.rodriguez.iniciosesion
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN=123
+    val COD_LOGOUT=323
+    private val TAG = "MyActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        var sign_in_button: SignInButton =findViewById(R.id.sign_in_button)
+        sign_in_button.setOnClickListener{
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+
+        if(requestCode == COD_LOGOUT){
+            signOut()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
+                Toast.makeText(this, "sesión terminada", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if(account!=null){
+            val intent = Intent(this, PrincipalActivity::class.java)
+            intent.putExtra("name", account.displayName)
+            intent.putExtra("email", account.email)
+            startActivityForResult(intent, COD_LOGOUT)
+        }
+
+    }
+}
---
+++ app/src/main/java/alexandra/rodriguez/iniciosesion/PrincipalActivity.kt
@@ -0,0 +1,30 @@
+package alexandra.rodriguez.iniciosesion
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+
+        val bundle = intent.extras
+        val tv_nombre: TextView = findViewById(R.id.tv_nombre)
+        val email: TextView = findViewById(R.id.tv_email)
+        val btn_cerrar:Button = findViewById(R.id.btn_cerrar)
+
+        if(bundle!=null){
+            val nombre = bundle.getString("name")
+            val correo = bundle .getString("email")
+            tv_nombre.setText(nombre)
+            email.setText(correo)
+        }
+
+        btn_cerrar.setOnClickListener {
+            finish()
+        }
+    }
+}
